791fb25ef8f8291e4d1e2d0cee75dbbc37e3751e
Cristian Diaz <diazcristian0210@gmail.com>
2024-08-26 19:30:23 -0300

    comenzamos a ver clase persona en java

--- diazCristian/Java/Leccion03/leccionClasesObjetos/src/Clases/Persona.java
+++ diazCristian/Java/Leccion03/leccionClasesObjetos/src/Clases/Persona.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package Clases;
 
-/**
- *
- * @author CristianPc
- */
 public class Persona {
     
 }
