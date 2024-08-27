946ada262201b8980d8a21d0a387f99f75667425
Héctor Cortez <updown.input@gmail.com>
2024-08-27 11:57:32 -0300

    Case 1: uso de listas, tuplas y rangos. Ejercicio 1, 2 y 3 de rangos y ejercicio 4 de tuplas y listas

---
+++ cortezHector/Python/Clase1/Ejercicio2.py
@@ -0,0 +1,5 @@
+# Ejercicio 2: Crear un rango de número de 2 a 6 e imprimirlos
+# Ejemplo: 2, 3, 4, 5, 6
+
+for i in range(2, 7):
+    print(i, end=" ")
---
+++ cortezHector/Python/Clase1/Ejercicio3.py
@@ -0,0 +1,6 @@
+# Ejercicio 3: Crear un rango de 3 a 10 con incremento de 2 en 2
+# Ejemplo: 3, 5, 7, 9
+
+for i in range(3, 11):
+    if i % 2 != 0:
+        print(i, end=" ")
