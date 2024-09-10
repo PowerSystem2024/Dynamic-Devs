6c461823f7db179bfc834dc0b4496d9fe538e2b5
Cristian Diaz <diazcristian0210@gmail.com>
2024-09-10 11:44:17 -0300

    agregamos la estructura del proyecto

---
+++ Tecnicatura/Portafolio/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import NavBar from './components/icons/NavBar.vue';
+</script>
+
+<template>
+  <header>
+    <!-- Barra de navegacion -->
+    <NavBar></NavBar>
+  </header>
+  <main>
+    <section id="educacion"><h2>Educacion - Cursos</h2></section>
+    <section id="experiencia"><h2>Experiencia</h2></section>
+    <section id="proyectos"><h2>Proyectos</h2></section>
+    <section id="habilidades"><h2>Habilidades</h2></section>
+    <section id="intereses"><h2>Intereses</h2></section>
+  </main>
+</template>
+
+<style scoped>
+ 
+</style>
---
+++ Tecnicatura/Portafolio/src/components/icons/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="navbar">
+      <div class="navbar-menu">
+        <!-- <ul> vfor con una key -->
+            <ul>
+             <a v-for="nav in navegacion" :key="nav.nombre" href="nav.enlace" class="navbar-item">{{ nav.nombre }}</a>
+            </ul>   
+      </div>  
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const navegacion = ref([
+    { id:1,nombre: "Educacion", enlace: "#educacion"},
+    { id:2, nombre: "Experiencia", enlace: "#experiencia"},
+    { id:3, nombre: "Proyectos", enlace: "#proyectos"},
+    { id:4, nombre: "Habilidades", enlace: "#habilidades"}
+]);
+</script>
+
+<style scoped>
+ .navbar{
+    background-color: var(--vt-c-indigo);
+    color: #fff;
+    padding: 0.5rem 1rem;
+    align-items: center;
+  }
+  .navbar-item{
+    color: #fff;
+    text-decoration: none;
+    margin-right: 1rem
+  }
+  .navbar-menu{
+    display: flex;
+    justify-content: flex-end;
+  }
+  .nav-list{
+    list-style: none;
+  }
+</style>
