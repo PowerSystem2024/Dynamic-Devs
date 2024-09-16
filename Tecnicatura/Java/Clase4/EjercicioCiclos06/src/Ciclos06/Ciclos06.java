e0ffbb1bb444c9d47801aa2b3c8a26ba81565dbb
Pablo Mateos <pablomateos19@gmail.com>
2024-09-16 15:19:07 -0300

    Java: Clase 4- Ejercicios 6 y 7 Ciclos (Clase Scanner y JOptionPane)

---
+++ Tecnicatura/Java/Clase4/EjercicioCiclos06/src/Ciclos06/Ciclos06.java
@@ -0,0 +1,21 @@
+/*
+Ejercicio 6: Pedir numeros hasta que se teclee un 0, mostrar
+la suma de todos los numeros introducidos.
+*/
+package Ciclos06;
+
+import java.util.Scanner;
+
+public class Ciclos06 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        int numero, suma =0;
+        do{
+            System.out.println("Digite un numero: ");
+            numero = Integer.parseInt(entrada.nextLine());
+            suma += numero;
+        }while(numero !=0);
+        System.out.println("\nLa suma de todos los numeros ingresados es: "+suma);    
+    }
+    
+}
