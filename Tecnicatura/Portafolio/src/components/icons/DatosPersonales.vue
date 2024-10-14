41ce1c8346abe8f40fe245cc11491f1b3f573718
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-14 18:43:30 -0300

    agregamos las diferentes areas de nuestros datos personales

--- Tecnicatura/Portafolio/src/components/icons/DatosPersonales.vue
+++ Tecnicatura/Portafolio/src/components/icons/DatosPersonales.vue
@@ -2,8 +2,20 @@
 </script>
 
 <template>
-
-
+<section class="datos-personales">
+    <div class="card">
+        <h1>Dynamic Devs</h1>
+        <h2>Técnico Universitario en Programacion - UTN</h2>
+        <p>Hola, Bienvenidos a nuestro portafolio de proyectos, somos un grupo de desarrolladores web con experiencia en el desarrollo de aplicaciones web y móviles</p>
+        <ul class="container-lista">
+            <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">Linkedin</a></li>
+            <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">Instagram</a></li>
+            <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">Github</a></li>
+            <li><a href=""><img class="icon-redsocial" src="" width="35rem" alt="">Curriculum vitae</a></li>
+        </ul>
+        <h3>Nuestro telefono de contacto es +54 9 2604-474851</h3>
+    </div>
+</section>
 </template>
 
 <style scoped>
