6b78184f685709f9f2fe87ac50e43ff15fdaca0d
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-14 18:59:26 -0300

    agregamos la estructura de datos personales

---
+++ Tecnicatura/Portafolio/src/components/DatosPersonales.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+<template>
+    <section>
+        <div>
+            <h1>Dynamic Devs</h1>
+            <h2>Tecnico Universitario en Programación</h2>
+            <p>Hola, bienvenidos a nuestro portafolio de proyectos, somos un grupo de desarrolladores web con experiencia en el desarrollo de aplicaciones web y móviles.</p>
+            <ul>
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem"alt="">Linkedin</a></li>
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem"alt="">Instagram</a></li>
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem"alt="">Github</a></li>
+                <li><a href=""><img class="icon-redsocial" src="" width="35rem"alt="">Curriculum vitae</a></li>
+            </ul>
+            <h3>Nuestro telefono: +54 9 2604-474849</h3>
+            <h4>San Rafael, Mnedoza, Argentina</h4>
+        </div>
+    </section>
+</template>
+<style scoped>
+</style>
