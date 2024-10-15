f2e5f301f85318ffc1eacbd3daae64cb402b592c
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-15 11:01:25 -0300

    Agregamos el template

--- Tecnicatura/Portafolio/src/components/ExperienciaComponente.vue
+++ Tecnicatura/Portafolio/src/components/ExperienciaComponente.vue
@@ -1,2 +1,31 @@
 <script setup>
 </script>
+
+<template>
+    <div class="card">
+        <h3 class="titulo">{{ titulo.toLocaleUpperCase() }}</h3>
+        <p class="fecha">{{ fecha }}</p>
+        <ul class="listado">
+            <li class="item" v-for="experencia in experiencias" :key="experencia.id">
+                <img class="imagen-svg" :src="experencia.src" width="45rem" :alt="experencia.parrafo">
+                <p>{{ experencia.parrafo }}</p>
+            </li>
+
+            <!-- <li class="item"><img class="imagen-svg" src="/src/assets/ventas.svg" width="45rem" alt="">
+                <p>Lideré un equipo de 5 desarrolladores en la creación de una plataforma de comercio electrónico que incrementó las ventas en un 35% durante el primer año.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/ecomerce.svg" width="45rem" alt="">
+                <p>Diseñé y desarrollé aplicaciones web utilizando Node.js, React y MongoDB, logrando reducir el tiempo de carga de las páginas en un 50%.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/configuracion.svg" width="45rem" alt="">
+                <p>Implementé soluciones de autenticación y autorización, mejorando la seguridad del sitio web y reduciendo los intentos de acceso no autorizado en un 20%.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/uiux.svg" width="45rem" alt="">
+                <p>Colaboré con diseñadores de UX/UI para crear interfaces de usuario atractivas y fáciles de usar, aumentando la satisfacción del cliente en un 15%.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/watch.svg" width="45rem" alt="">
+                <p>Automaticé procesos de despliegue continuo utilizando Jenkins y Docker, reduciendo los tiempos de despliegue en un 40%.</p>
+            </li> -->
+        </ul>
+    </div>
+</template>
