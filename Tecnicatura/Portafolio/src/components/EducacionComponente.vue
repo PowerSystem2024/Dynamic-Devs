5f0d33dd88905352abab28de3b4422747a456b70
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-15 10:46:06 -0300

    Solucion de tipeo en el apartado fechas

--- Tecnicatura/Portafolio/src/components/EducacionComponente.vue
+++ Tecnicatura/Portafolio/src/components/EducacionComponente.vue
@@ -11,10 +11,10 @@ fechaColor.value = [
 ];
 /*Esta es la forma de utilizar el arreglo, sin el metodo value*/
 const educacion = ref([
-  {fecha: '2024', title: 'Técnicatura Universitaria en Programacion', descripcion: 'Incumbencias Profesionales: Operación y programación de computadoras, desarrollo de programas en distintos lenguajes, análisis y control de sistemas informáticos.', enlace:'https://www.youtube.com/'},
+  {fecha: '2024', title: 'Técnicatura Universitaria en Programacion UTN San Rafael', descripcion: 'Trabajamos con metodologias scrum, aprendimos Python, Java, Javascript, MYSQL entre otros lenguajes.', enlace:'https://www.youtube.com/'},
   {fecha: '2023', title: 'Desarrollador Full Stack', descripcion: 'Trabajé en RollingCode, donde diseñé y desarrollé aplicaciones web completas utilizando tecnologías como Node.js, React y MongoDB.', enlace:'http:www.direccion.com'},
   {fecha: '2022', title: 'Internship en Desarrollo Web', descripcion: 'Realicé una pasantía en ABC Solutions, contribuyendo en la creación de interfaces de usuario y optimización de sitios web.', enlace:'http:www.direccion.com'},
-  {fecha: '2021', title: 'Proyecto Personal - Aplicación de Gestión de Tareas', descripcion: 'Desarrollé una aplicación para la gestión de tareas diarias usando HTML, CSS y JavaScript, implementando funcionalidades como listas de tareas y recordatorios.', enlace:'http:www.direccion.com'},
+  {fecha: '2021', title: 'Proyecto Personal - Aplicación de Gestión de Tareas', descripcion: 'Desarrollamos una aplicación para la gestión de tareas diarias usando HTML, CSS y JavaScript, implementando funcionalidades como listas de tareas y recordatorios.', enlace:'http:www.direccion.com'},
   {fecha: '2020', title: 'Curso de Introducción a la Programación', descripcion: 'Completé un curso en línea sobre fundamentos de programación, donde aprendí lenguajes como Python y Java.', enlace:'http:www.direccion.com'}
 ]);
 </script>
