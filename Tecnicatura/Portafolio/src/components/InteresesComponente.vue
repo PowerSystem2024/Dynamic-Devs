42b6d65a4e7aba14a7c8d34f7715c02920db26cc
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-21 18:27:11 -0300

    Agregamos el template en intereses componente

--- Tecnicatura/Portafolio/src/components/InteresesComponente.vue
+++ Tecnicatura/Portafolio/src/components/InteresesComponente.vue
@@ -11,7 +11,34 @@ const intereses = ref([
 </script>
 
 <template>
-
+      <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+    <!-- <div class="intereses-contenedor">
+        <ul class="contenedor-lista">
+            <li class="item">
+                Desarrollo de Software de Código Abierto: Contribuyo a proyectos en GitHub, colaborando con otros
+                desarrolladores para mejorar herramientas y librerías populares.
+            </li>
+            <li class="item">
+                Deportes al Aire Libre: Disfruto del senderismo y el ciclismo de montaña, actividades que practico
+                regularmente para mantener un equilibrio entre el trabajo y la vida personal.
+            </li>
+            <li class="item">
+                Tecnología de Innovación: Me apasiona explorar nuevas tendencias tecnológicas como la inteligencia
+                artificial y el desarrollo de aplicaciones móviles.
+            </li>
+            <li class="item">
+                Fotografía: Practico la fotografía como un hobby, lo que me ayuda a mantener una perspectiva creativa
+                tanto en mi vida profesional como personal.
+            </li>
+        </ul>
+    </div> -->
 </template>
 
 <style scoped>
