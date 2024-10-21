7217fda9b9a5dbc7d3afd83fde1eb72b6f22579e
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-21 18:22:48 -0300

    Agregamos el script con el import del fondoLava

--- Tecnicatura/Portafolio/src/components/InteresesComponente.vue
+++ Tecnicatura/Portafolio/src/components/InteresesComponente.vue
@@ -1,5 +1,13 @@
 <script setup>
-
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+//Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Desarrollo de Software de Código Abierto: Contribuyo a proyectos en GitHub, colaborando con otros desarrolladores para mejorar herramientas y librerías populares.',
+    'Deportes al Aire Libre: Disfruto del senderismo y el ciclismo de montaña, actividades que practico regularmente para mantener un equilibrio entre el trabajo y la vida personal.',
+    'Tecnología de Innovación: Me apasiona explorar nuevas tendencias tecnológicas como la inteligencia artificial y el desarrollo de aplicaciones móviles.',
+    'Fotografía: Practico la fotografía como un hobby, lo que me ayuda a mantener una perspectiva creativa tanto en mi vida profesional como personal.'
+]);
 </script>
 
 <template>
